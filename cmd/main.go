52d7e0fd247444f48ddd1b36254918dde77d6be7
Sylvain Gaunet <1552102+sgaunet@users.noreply.github.com>
2025-05-15 21:43:40 +0200

    chore: add linting and CI/CD workflows

--- cmd/main.go
+++ cmd/main.go
@@ -1,16 +1,22 @@
+// Package main implements a command line tool to retry a command
 package main
 
 import (
 	"flag"
 	"fmt"
 	"log/slog"
+	"math"
 	"os"
 	"time"
 
 	"github.com/sgaunet/retry/pkg/retry"
 )
 
-var version = "dev"
+var (
+	version                        = "dev"
+	defaultMaxTries           uint = 3
+	defaultSleepTimeInSeconds uint
+)
 
 func printVersion() {
 	fmt.Printf("%s\n", version)
@@ -24,8 +30,8 @@ func main() {
 		printVersionFlag   bool
 		helpFlag           bool
 	)
-	flag.UintVar(&maxtries, "m", 3, "max tries of execution of failed command (0 for infinite tries)")
-	flag.UintVar(&sleepTimeInSeconds, "s", 0, "sleep time in seconds between each try")
+	flag.UintVar(&maxtries, "m", defaultMaxTries, "max tries of execution of failed command (0 for infinite tries)")
+	flag.UintVar(&sleepTimeInSeconds, "s", defaultSleepTimeInSeconds, "sleep time in seconds between each try")
 	flag.StringVar(&command, "c", "", "command to execute")
 	flag.BoolVar(&printVersionFlag, "version", false, "print version")
 	flag.BoolVar(&helpFlag, "h", false, "print help")
@@ -56,7 +62,12 @@ func main() {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
-	r.SetSleep(func() { time.Sleep(time.Duration(sleepTimeInSeconds) * time.Second) })
+	if sleepTimeInSeconds > uint(math.MaxInt64) {
+		fmt.Fprintln(os.Stderr, "sleep time too large")
+		os.Exit(1)
+	}
+	r.SetSleep(func() { time.Sleep(time.Duration(int64(sleepTimeInSeconds)) * time.Second) }) //nolint:gosec
+
 	err = r.Run(logger)
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err)
